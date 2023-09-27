7a798aea9803f4f24e1e3ab9f8d1c1056cbfc93d
Austin Dobberfuhl <AUSTIN.DOBBERFUHL4597@STU.MTEC.EDU>
2023-09-27 10:01:51 -0600

    Initial Commit

---
+++ TextValidation App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextValidation App
+//
+//  Created by Austin Dobberfuhl on 9/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
